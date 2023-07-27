d4a951aa761176ee991cd89907609687700f90cb
Pratik <pendurkarpratik04@gmail.com>
2023-07-27 11:38:53 +0530

    recursion

---
+++ recursion/factorial.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact(int num)
+{
+    if (num == 0 or num == 0)
+        return 1;
+    return num * fact(num - 1);
+}
+int main()
+{
+    int num;
+    cin >> num;
+    cout << fact(num);
+    return 0;
+}
